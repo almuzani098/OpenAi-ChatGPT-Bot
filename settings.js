1a0f339e99f1cfaa18764f3647c1e074c0b2acc6
Al muzani <105990040+almuzani098@users.noreply.github.com>
2024-10-02 23:01:29 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -17,7 +17,7 @@ global.ONLY_GROUPS = false // Only Groups Use
 🤖 𝗕𝗢𝗧 𝗜𝗡𝗙𝗢 𝗦𝗘𝗧𝗧𝗜𝗡𝗚𝗦
 */
 global.OWNER_NAME = 'Dark Maker Ofc' // Owner Name
-global.OWNER_NUMBER = ['94715166712'] //Owner Number
+global.OWNER_NUMBER = ['6283141929853'] //Owner Number
 global.BOT_ADMINS = ['94715166712','6283141929853'] //Bot admins numbers Like a Sudo Numbers
 global.BLOCK_CHAT = [] // If you want Block Bot From Chats Use .jid Command and put Jids
 global.IMAGE_QUALITY = 'low' // Available Quality Are => low | mediam | high
